369d09ca791e2d6c631b4fe1ae722095e34c3464
Maggie <mmf295@icloud.com>
2024-04-19 10:55:29 -0400

    add JS for sub-accordions, style content

--- nursing.js
+++ nursing.js
@@ -1,10 +1,25 @@
 console.clear();
 
+function closeCurrentAccordion(accordion, tween) {
+    accordion.classList.remove('active');
+    tween.reverse();
+};
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        if (currentItem !== null) {
+            closeCurrentAccordion(items[currentItem], items[currentItem]._accordionTween);
+            currentItem = null;
+        }
+    }
+});
+
 const items = gsap.utils.toArray(".item");
 let currentItem = null;
 
 items.forEach((e, i) => {
-    const content = e.querySelector(".section-content");
+    const header = e.querySelector(".section-header");
+    const content = e.querySelector('.section-content')
     const t = gsap.to(content, {
         height: 'auto',
         duration: 1,
@@ -14,17 +29,40 @@ items.forEach((e, i) => {
 
     e._accordionTween = t;
 
-    e.addEventListener('click', () => {
+    header.addEventListener('click', () => {
         if (currentItem !== null) {
-            items[currentItem].classList.toggle('active');
+            closeCurrentAccordion(items[currentItem], items[currentItem]._accordionTween);
             if (currentItem === i) {
                 currentItem = null;
-                return t.reverse();
+                return;
             }
-            items[currentItem]._accordionTween.reverse();
         }
-        e.classList.toggle('active');
+        e.classList.add('active');
         t.play();
         currentItem = i;
+    });
+});
+
+const subItems = gsap.utils.toArray('.sub-item');
+
+subItems.forEach((subItem) => {
+    const subHeader = subItem.querySelector('.sub-title');
+    const subContent = subItem.querySelector('.sub-content');
+    const subTween = gsap.to(subContent, {
+        height: 'auto',
+        duration: 0.5,
+        ease: 'expo.out',
+        paused: true
+    });
+
+    subHeader.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const isActive = subItem.classList.contains('active');
+        subItem.classList.toggle('active', !isActive);
+        if (isActive) {
+            subTween.reverse();
+        } else {
+            subTween.play()
+        }
     });
 });
